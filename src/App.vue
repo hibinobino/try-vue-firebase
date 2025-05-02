7698f3ddb3db7557dd3983fffdcce619249aa6cf
hibinobino <ayako.hibino@gmail.com>
2025-05-02 19:17:48 +0900

    firebase script

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+/*---firebase---*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGtWbXsHGQ3kKoahKhwljnTZjjBp9wFmE",
+  authDomain: "try-vue-firebase-8bf69.firebaseapp.com",
+  projectId: "try-vue-firebase-8bf69",
+  storageBucket: "try-vue-firebase-8bf69.firebasestorage.app",
+  messagingSenderId: "52805983968",
+  appId: "1:52805983968:web:334b2cd4bdfbf3e70a7a85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 </script>
 
 <template>
