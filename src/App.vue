5fdb7fa03f5e5d607b053c6e68f287b43026a2b6
hibinobino <ayako.hibino@gmail.com>
2025-05-03 19:56:08 +0900

    add user data update on login

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <LoginScreen v-if="!isLoggedIn" />
-  <UserScreen :db="db" v-if="isLoggedIn" />
+  <UserScreen :db="db" :userUid="userUid" v-if="isLoggedIn" />
   <LogoutScreen v-if="isLoggedIn" />
 </template>
 
 <script setup>
-/* ---Vue Component--- */
 import LoginScreen from "./components/LoginScreen.vue"
 import LogoutScreen from "./components/LogoutScreen.vue"
 import UserScreen from "./components/UserScreen.vue"
-
 import { ref } from "vue";
-
-/* ---Firebase--- */
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {
@@ -20,6 +16,11 @@ import {
   onAuthStateChanged
 } from "firebase/auth";
 
+const auth = getAuth()
+const isLoggedIn = ref(false);
+const userUid = ref("")
+
+//Firebase準備
 const firebaseConfig = {
   apiKey: "AIzaSyDGtWbXsHGQ3kKoahKhwljnTZjjBp9wFmE",
   authDomain: "try-vue-firebase-8bf69.firebaseapp.com",
@@ -28,18 +29,17 @@ const firebaseConfig = {
   messagingSenderId: "52805983968",
   appId: "1:52805983968:web:334b2cd4bdfbf3e70a7a85"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const isLoggedIn = ref(false);
-const auth = getAuth()
-
+//ログイン状態を確認
 onAuthStateChanged(auth, (user) => {
   //console.log("checkAuthState: " + user);
   if (user) {
+    userUid.value = user.uid
     isLoggedIn.value = true;
   } else {
+    userUid.value = ""
     isLoggedIn.value = false;
   }
 });
--- src/components/UserScreen.vue
+++ src/components/UserScreen.vue
@@ -6,18 +6,22 @@
 </template>
 
 <script setup>
+import { getAuth } from "firebase/auth";
 import {
   collection,
   doc,
   Firestore,
   setDoc,
   Timestamp,
-  getDoc
+  getDoc,
+  updateDoc,
 } from "firebase/firestore";
 
 const props = defineProps({
   db: Firestore,
+  userUid: String,
 });
+const auth = getAuth();
 
 // Add a new document in collection "cities"
 const addDocument = () => {
@@ -33,14 +37,37 @@ const addDocument = () => {
     });
 };
 const getDocument = async () => {
-  const docSnap = await getDoc(doc(props.db,"test-collection", "test-doc"))
+  const docSnap = await getDoc(doc(props.db, "test-collection", "test-doc"));
   if (docSnap.exists()) {
-  alert("Document data:", docSnap.data());
-} else {
-  // docSnap.data() will be undefined in this case
-  alert("No such document!");
-}
+    alert("Document data:", docSnap.data());
+  } else {
+    // docSnap.data() will be undefined in this case
+    alert("No such document!");
+  }
 };
+
+//初回ログイン時、usersコレクションにユーザーを追加する
+created(() => {
+  getDoc(doc(props.db, "users", props.uid)) //ユーザーデータを取得する
+    .then((userDoc) => {
+      //取得できればデータを更新する
+      updateDoc(userDoc, {
+        loginDateTime: Timestamp.now(), //ログイン日時
+      }).then(()=>{
+        console.log("updated user data: loginDateTime.")
+      })
+    })
+    .error(() => {
+      //取得できなかったらデータを新規に追加する
+      setDoc(doc(props.db, "users", props.uid), {
+        email: auth.getUser(props.uid).email,
+        createdDateTime: Timestamp.now(),
+        loginDateTime: Timestamp.now(),
+      }).then(()=>{
+        console.log("created user data.")
+      });
+    });
+});
 </script>
 
 <style scoped></style>
