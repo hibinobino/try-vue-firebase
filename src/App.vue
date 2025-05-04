061ec2b4310e471e457b128b736fb3dad5c12326
hibinobino <ayako.hibino@gmail.com>
2025-05-04 15:52:35 +0900

    update: Login page ui

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <LoginScreen v-if="!isLoggedIn" />
-  <UserScreen :db="db" :userUid="userUid" v-if="isLoggedIn" />
-  </template>
+  <div class="font-sans">
+    <LoginScreen v-if="!isLoggedIn" />
+    <UserScreen :db="db" :userUid="userUid" v-if="isLoggedIn" />
+  </div>
+</template>
 
 <script setup>
-import LoginScreen from "./components/LoginScreen.vue"
-import UserScreen from "./components/UserScreen.vue"
+import LoginScreen from "./components/LoginScreen.vue";
+import UserScreen from "./components/UserScreen.vue";
 import { ref } from "vue";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  getAuth,
-  onAuthStateChanged
-} from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const isLoggedIn = ref(false);
-const userUid = ref("")
+const userUid = ref("");
 
 //Firebase準備
 const firebaseConfig = {
@@ -24,25 +23,23 @@ const firebaseConfig = {
   projectId: "try-vue-firebase-8bf69",
   storageBucket: "try-vue-firebase-8bf69.firebasestorage.app",
   messagingSenderId: "52805983968",
-  appId: "1:52805983968:web:334b2cd4bdfbf3e70a7a85"
+  appId: "1:52805983968:web:334b2cd4bdfbf3e70a7a85",
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth()
+const auth = getAuth();
 
 //ログイン状態を確認
 onAuthStateChanged(auth, (user) => {
   //console.log("checkAuthState: " + user);
   if (user) {
-    userUid.value = user.uid
+    userUid.value = user.uid;
     isLoggedIn.value = true;
   } else {
-    userUid.value = ""
+    userUid.value = "";
     isLoggedIn.value = false;
   }
 });
-
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/LoginScreen.vue
+++ src/components/LoginScreen.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <GoogleButton @click="googleLogin" />
+  <div class="h-screen w-screen flex justify-center items-center bg-violet-100">
+    <div class="shadow-2xl rounded-lg w-xl m-auto bg-white p-10 space-y-10">
+      <div class="flex justify-center"><h1 class="text-6xl">Notion-like App</h1></div>
+      <div class="flex justify-center"><GoogleButton @click="googleLogin" /></div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "firebase/auth";
-import GoogleButton from "./GoogleButton.vue"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import GoogleButton from "./GoogleButton.vue";
 
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
@@ -21,17 +20,16 @@ const googleLogin = () => {
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       const user = result.user;
-      console.log("Login successful.")
+      console.log("Login successful.");
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       const email = error.customData.email;
       const credential = GoogleAuthProvider.credentialFromError(error);
-      console.log("Login failed.")
+      console.log("Login failed.");
     });
 };
-
 </script>
 
 <style scoped></style>
