d4a9429821a1caa6ce379a27ad3b1c4e19956ac1
hibinobino <ayako.hibino@gmail.com>
2025-05-06 23:33:59 +0900

    ページ要素テスト中

--- src/components/Right/PageContent.vue
+++ src/components/Right/PageContent.vue
@@ -4,6 +4,34 @@
     <input
       type="text"
       class="focus:outline-none focus:ring-0 text-4xl font-bold h-12 selection:bg-blue-200"
+      v-model="name"
+    />
+    <div class="text-sm text-gray-400 font-bold">
+    <div>Created: 2025-01-01 12:00</div>
+    <div>Updated: 2025-01-01 12:00</div>
+    </div>
+    
+    <input
+      type="text"
+      class="focus:outline-none focus:ring-0 font-bold selection:bg-blue-200"
+      v-model="name"
+    />
+    <input
+      type="text"
+      class="focus:outline-none focus:ring-0 font-bold selection:bg-blue-200"
+      v-model="name"
+    />
+    <input
+      type="text"
+      class="focus:outline-none focus:ring-0 font-bold selection:bg-blue-200"
+      v-model="name"
     />
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = ref("New Page")
+
+</script>
