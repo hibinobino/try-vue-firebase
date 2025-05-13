dd972beff41119b322244a61bc20cc46f1a27ec7
hibinobino <ayako.hibino@gmail.com>
2025-05-13 21:18:52 +0900

    PageContentコンポーネントにloadPageData追加

--- src/components/Right/PageContent.vue
+++ src/components/Right/PageContent.vue
@@ -3,26 +3,28 @@
     <!--ページタイトル-->
     <div
       class="focus:outline-none focus:ring-0 text-4xl font-bold h-12 selection:bg-blue-200"
-      contenteditable="true">title text</div>
+      contenteditable="true"
+    >
+      title text
+    </div>
     <div class="text-sm text-gray-400 font-bold">
       <div>Created: 2025-01-01 12:00</div>
       <div>Updated: 2025-01-01 12:00</div>
     </div>
-
   </div>
 </template>
 
 <script setup>
-import { DocumentSnapshot } from "firebase/firestore";
+import { doc, DocumentSnapshot, Firestore, getDoc } from "firebase/firestore";
 import { Marked } from "marked";
 import { ref, watch } from "vue";
 
 //親コンポーネントから受け取るページの内容
 //読み取り専用
 const props = defineProps({
-  currentPageId: String
+  db: Firestore,
+  currentPageId: String,
 });
-
 
 //ページデータを格納する
 const pageData = ref(DocumentSnapshot);
@@ -30,10 +32,22 @@ const pageData = ref(DocumentSnapshot);
 //mdContentをMarkDownに変換する用
 const mdParser = new Marked();
 
-//ページが変わるとcurrentPageIdが変わるので
-//その都度内容を読み込みなおす
-watch(currentPageId, (newPageId) => {
-  alert("Opened new page: "+newPageId+"\nOld page was: "+currentPageId)
-
+//currentPageIdが変わったらページを再読み込みする
+watch(props.currentPageId, (newPageId) => {
+  alert("Opened new page: " + newPageId + "\nOld page was: " + currentPageId);
+  loadPageData()
 });
+
+//ページ読み込み
+const loadPageData = (pageId) => {
+  const docRef = doc(props.db, "pages", pageId);
+
+  getDoc(docRef)
+    .then((docSnap) => {
+      pageData.value = docSnap.data();
+    })
+    .catch((error) => {
+      console.log("loadPageData failed.");
+    });
+};
 </script>
