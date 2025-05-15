30355d2ad6023eedc96a353a32cc31b0017222d7
hibinobino <ayako.hibino@gmail.com>
2025-05-15 19:35:14 +0900

    md入力奮闘中

--- src/components/Right/PageMdContent.vue
+++ src/components/Right/PageMdContent.vue
@@ -1,33 +1,46 @@
 <template>
-  <div id="md-content-area" spellcheck="false"  ref="refMdContent" @input="mdContentEdit">
+  <div
+    id="md-content-area"
+    spellcheck="false"
+  >
     <div
       contenteditable="true"
-      class="focus:outline-none focus:ring-0"
+      class="focus:outline-none focus:ring-0 bg-red-100"
+    ref="refMdContent"
+    @input="mdContentEdit"
     >{{ mdContentText }}</div>
+    <div
+      v-html="mdContentParsed"
+      class="focus:outline-none focus:ring-0 bg-blue-100"
+    ></div>
   </div>
 </template>
 <script setup>
 import { Marked } from "marked";
+import { split } from "postcss/lib/list";
 import { ref, watch } from "vue";
 
 const props = defineProps({
   mdContent: String,
 });
 
-const emits = defineEmits(["mdcontent-edit"])
-const refMdContent = ref(null)
+const emits = defineEmits(["mdcontent-edit"]);
+const refMdContent = ref(null);
 
-const mdContentText = ref("")
-const mdContentRendered = ref("")
+const mdContentText = ref("");
+const mdContentLines = ref(Array<String>[])
+const mdContentParsed = ref("");
 
 //mdContentをMarkDownに変換する用
 const mdParser = new Marked();
 
-const mdContentEdit = () =>{
-    
-}
+const mdContentEdit = (e) => {
+  mdContentText.value = refMdContent.value.innerText;
+  //mdContentLines.value=split(mdContentText.value,)
+  mdContentParsed.value = mdParser.parse(mdContentText.value);
+};
 
-watch(props,(newP,oldP)=>{
-    mdContentText.value  = newP.title
-})
+watch(props, (newP, oldP) => {
+  mdContentText.value = newP.title;
+});
 </script>
