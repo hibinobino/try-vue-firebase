9f8a26f8efb23b7792a6af7462d8786ed13684cf
hibinobino <ayako.hibino@gmail.com>
2025-05-15 10:57:38 +0900

    タイトル部分をコンポーネント化、編集をリアルタイムに送信

--- src/components/Right/PageContent.vue
+++ src/components/Right/PageContent.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="right section">
     <!--ページタイトル-->
-    <div
-      class="focus:outline-none focus:ring-0 text-4xl font-bold h-12 selection:bg-blue-200"
-      contenteditable="true"
-    >
-      {{ name }}
-    </div>
+    <PageTitle :title="name" @title-edit="updateTitle" />
     <div class="text-sm text-gray-400 font-bold">
       <div>Created: 2025-01-01 12:00</div>
       <div>Updated: 2025-01-01 12:00</div>
@@ -15,8 +10,13 @@
       v-html="mdParser.parse(mdContent)"
       contenteditable="true"
       class="focus:outline-none focus:ring-0"
-    ></div><div
-      v-html="mdParser.parse('# This is an H1\n\n## This is an H2\n\n###### This is an H6')"
+    ></div>
+    <div
+      v-html="
+        mdParser.parse(
+          '# This is an H1\n\n## This is an H2\n\n###### This is an H6'
+        )
+      "
       contenteditable="true"
       class="focus:outline-none focus:ring-0"
     ></div>
@@ -24,9 +24,16 @@
 </template>
 
 <script setup>
-import { doc, DocumentSnapshot, Firestore, getDoc } from "firebase/firestore";
+import {
+  doc,
+  DocumentSnapshot,
+  Firestore,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { Marked } from "marked";
 import { onMounted, ref, watch } from "vue";
+import PageTitle from "./PageTitle.vue";
 
 //親コンポーネントから受け取るページの内容
 //読み取り専用
@@ -76,9 +83,14 @@ watch(props, (newP, oldP) => {
   loadPageData(newP.currentPageId);
 });
 
-/*離脱時の処理を調べる
-onbeforeunload(() => {
-  alert("unmounttest")
-  //今のページを保存する
-});*/
+//書き換えられたページタイトルをFirestoreに送信する
+const updateTitle = (titleText) => {
+  updateDoc(doc(props.db, "pages", props.currentPageId), {
+    name: titleText,
+  }).then(() => {
+
+  }).catch((error)=>{
+    console.log("error updateTitle: " + error.message)
+  });
+};
 </script>
---
+++ src/components/Right/PageTitle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+      class="focus:outline-none focus:ring-0 text-4xl font-bold h-12 selection:bg-blue-200"
+      contenteditable="true"
+      ref="refTitle"
+      @input="titleEdit"
+    >
+      {{ titleText }}
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+    title: String
+})
+const emits = defineEmits(["title-edit"])
+const refTitle = ref(null)
+const titleText = ref("")
+
+const titleEdit = ()=>{
+    //console.log(refTitle.value.innerText)
+    emits("title-edit",refTitle.value.innerText)
+}
+
+watch(props,(newP,oldP)=>{
+    titleText.value  = newP.title
+})
+</script>
